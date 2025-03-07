cdf33d59da5588e785725f1ff536de0543dfddb1
Sascha Rose <sascha.rose@gmail.com>
2025-03-07 08:15:02 +0100

    add camera

--- app/detail.jsx
+++ app/detail.jsx
@@ -1,8 +1,57 @@
 import { Text, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
 
 export default function DetailPage() {
   const { isbn } = useLocalSearchParams();
+  const [data, setData] = useState(null);
+
+  // https://openlibrary.org/api/books?bibkeys=ISBN:1931498717&jscmd=details&format=json
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=details&format=json`
+      );
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData();
+  }, [isbn]);
+
+  if (JSON.stringify(data) === "{}") {
+    return <Text>Das Buch wurde nicht gefunden!</Text>;
+  }
+
+  // const data = {
+  //   "ISBN:9783424631074": {
+  //     bib_key: "ISBN:9783424631074",
+  //     details: {
+  //       classifications: [Object],
+  //       created: [Object],
+  //       identifiers: [Object],
+  //       isbn_10: [Array],
+  //       key: "/books/OL31684942M",
+  //       languages: [Array],
+  //       last_modified: [Object],
+  //       latest_revision: 3,
+  //       number_of_pages: 276,
+  //       publish_date: "2015",
+  //       publishers: [Array],
+  //       revision: 3,
+  //       source_records: [Array],
+  //       subtitle: "Der Schlüssel zur Lösung (fast) aller Probleme",
+  //       title: "Das Kind in dir muss Heimat finden",
+  //       type: [Object],
+  //       works: [Array],
+  //     },
+  //     info_url:
+  //       "https://openlibrary.org/books/OL31684942M/Das_Kind_in_dir_muss_Heimat_finden",
+  //     preview: "noview",
+  //     preview_url:
+  //       "https://openlibrary.org/books/OL31684942M/Das_Kind_in_dir_muss_Heimat_finden",
+  //   },
+  // };
 
   return (
     <View>
--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,55 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { useState } from "react";
+import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
 
 export default function IndexPage() {
-  const [isbn, setIsbn] = useState("1234567890");
+  const [scannedCode, setScannedCode] = useState(0);
+  const [permission, requestPermission] = useCameraPermissions();
 
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View>
+        <Text>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function handleScannedResult(code) {
+    if (code != scannedCode) {
+      console.debug("Scanned code", code);
+      setScannedCode(code);
+    }
+  }
+
+  console.debug("render IndexPage");
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: "center" }}>
       <Text>IndexPage</Text>
-      <Link
-        href={{
-          pathname: "/detail",
-          params: { isbn },
-        }}
-      >
-        Link to Detail Page
-      </Link>
+
+      <CameraView
+        style={{ height: 400, width: "80%", margin: 20 }}
+        barcodeScannerSettings={{ barcodeTypes: ["ean13"] }}
+        onBarcodeScanned={({ data }) => handleScannedResult(data)}
+      />
+
+      {scannedCode ? (
+        <Link
+          href={{
+            pathname: "/detail",
+            params: { isbn: scannedCode },
+          }}
+        >
+          Link to Detail Page
+        </Link>
+      ) : (
+        <Text>scanning ...</Text>
+      )}
     </View>
   );
 }
